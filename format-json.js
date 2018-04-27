7db5dac64c3c8e7adf7dd3da7873533301b8aa0f
Pete Jansz <peterjansz@gmail.com>
2018-04-27 16:09:25 -0700

    Fixed! really works.

--- format-json.js
+++ format-json.js
@@ -1,7 +1,19 @@
-var lib1 = require( process.env.USERPROFILE + '/Documents/bin/lib1.js' )
-const fs = require( 'fs' )
+#! /usr/bin/env node
 
-const STDIN_FILENO = 0
-var stdinBuffer = fs.readFileSync( STDIN_FILENO ).toString().trim()
-var o = JSON.parse( stdinBuffer )
-console.log( JSON.stringify( o, null, 4 ) )
+// Format JSON from stdin
+
+var inputStream = process.stdin, data = ''
+
+process.stdin.resume()
+
+// Read the entire input stream into the data variable.
+inputStream.on( 'data', function ( chunk )
+{
+    data += chunk
+} )
+
+// At end of stream, load the JSON object and process it.
+inputStream.on( 'end', function ()
+{
+    console.log( JSON.stringify( JSON.parse( data ), null, 4 ) )
+} )
