cf31662c65c7f5db9900ef51a1cbb7b4320da831
Pete Jansz <peterjansz@gmail.com>
2018-08-16 10:08:52 -0700

    Add create note to processes-account.js

--- processes-account.js
+++ processes-account.js
@@ -1,36 +1,67 @@
 /*
-  NodeJS command-line interface to pd-crm-processess activate, close account REST methods.
+  NodeJS command-line interface to pd-crm-processess account (activate or close), add-note.
   Author: Pete Jansz
 */
 
 const modulesPath = '/usr/share/node_modules/'
 var program = require( modulesPath + 'commander' )
-var processes = require(modulesPath + 'pete-lib/processes-lib')
+var processes = require( modulesPath + 'pete-lib/processes-lib' )
 
 program
     .version( '0.0.1' )
-    .description( 'CLI to pd-crm-processess activate, close account REST methods' )
+    .description( 'CLI to pd-crm-processess account (activate or close), add-note' )
     .usage( ' -<activate|close> -i <playerId> -h <hostname>' )
     .option( '-a, --activate', 'Activate account' )
     .option( '-c, --close', 'Close account' )
     .option( '-i, --playerId <playerId>', 'PlayerID', parseInt )
     .option( '-h, --hostname <hostname>', 'Hostname' )
+    .option( '-n, --note', 'Create note' )
     .parse( process.argv )
 
 process.exitCode = 1
 
-if ( !program.playerId || !program.hostname || !(program.activate || program.close))
+if ( !program.playerId || !program.hostname || !( program.activate || program.close || program.note ) )
 {
     program.help()
 }
 
 var restPath, request
 
-if (program.activate || program.close)
+if ( program.activate || program.close )
 {
-    if (program.activate) { restPath = '/california/api/v1/processes/account-activation' }
-    else                  { restPath = '/california/api/v1/processes/close-account'}
-    request = createAccountRequest()
+    request = processes.createProcessesRequest()
+
+    if ( program.activate )
+    {
+        restPath = '/california/api/v1/processes/account-activation'
+        request.token = program.playerId
+    }
+    else
+    {
+        restPath = '/california/api/v1/processes/close-account'
+        request.playerId = program.playerId
+        request.reason = ': transactionTime: ' + reqData.transactionTime
+    }
+}
+else if ( program.note )
+{
+    restPath = '/california/api/v1/processes/player-note'
+    request = processes.createProcessesRequest()
+    request.playerId = program.playerId
+
+    const note =
+    {
+        alert: false,
+        id: null,
+        playerId: request.playerId,
+        status: 1,
+        type: 1,
+        value: 'Make a note @ ' + new Date(),
+        priority: 1,
+        user: 'administrator',
+    }
+
+    request.note = note
 }
 
 processes.createAxiosInstance( program.hostname, program.playerId ).
@@ -47,21 +78,3 @@ processes.createAxiosInstance( program.hostname, program.playerId ).
     } )
 
 // End main logic
-// Local functions
-function createAccountRequest( )
-{
-    var reqData = processes.createProcessesRequest()
-
-    if ( program.close )
-    {
-        reqData.playerId = program.playerId
-        reqData.reason = ': transactionTime: ' + reqData.transactionTime
-
-    }
-    else // activate
-    {
-        reqData.token = program.playerId
-    }
-
-    return reqData
-}
