23c8e22878a93576d52c37c837ff126df4ed379d
Pete Jansz <peterjansz@gmail.com>
2019-03-27 16:27:17 -0700

    Add XML formatting, time, date functions

--- lib-general.ps1
+++ lib-general.ps1
@@ -2,29 +2,99 @@
     Author: Pete Jansz, IGT, June 2017
 #>
 
-#$mailRegex = "^[^\\s].{0,64}@.{1,200}\\..{0,55}[^\\s]$"
-#$contractId = "^\\d{10}$"
-#$phone = "^\\d{10,14}$"
-#$PasswordRegex = '^((? = ".*\\d)(? = ".*[a-z])(? = ".*[A-Z]).{8,50})$'
-#$passwordPatterns = '[A-Z],[a-z],[0-9],[&%$#@!]'
 $addressLine = "^[a-zA-Z0-9 /.#-]{1,50}$"
 $addressLineNotRequired = "^[a-zA-Z0-9 /.#-]{0,50}$"
-#$city = "^[a-zA-Z0-9 ]{1,50}$"
-#$postalCode = "^\\d{5}(\\d{4})?$"
-#$isoCountryCode = "^[A-Z]{1,5}$"
-#$state = "^[A-Z]{1,3}$"
-
-
-
 $EmailRegex = "^[a-zA-Z0-9.!£#$%&'^_`{}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
 $FirstnameRegEx = "^[A-Z ,.'-]{1,30}$"
 $FirstnameRegEx = "^[A-Za-z\\xE1\\xE9\\xED\\xF3\\xFA\\xC1\\xC9\\xCD\\xD3\\xDA\\xF1\\xD1\\xDC\\xFC ,.'-]{1,30}$"
 $LastnameRegEx = "^[A-Za-z\\xE1\\xE9\\xED\\xF3\\xFA\\xC1\\xC9\\xCD\\xD3\\xDA\\xF1\\xD1\\xDC\\xFC ,.'-]{1,30}$"
-#$LastnameRegEx = $FirstnameRegEx
 
+function rmXmlDeclaration ([string]$filename)
+{
+    $absFilename = resolve-path $filename
+    [xml] $xmlDoc = Get-Content ($absFilename)
+    if ($xmlDoc.xml -match 'version=')
+    {
+        $xmlDoc.RemoveChild($xmlDoc.FirstChild) | out-null
+        $xmlDoc.save($absFilename)
+    }
+}
+function Format-XML ([string]$xmlfilename, [bool]$indent = $true, [int]$indentCount = 4)
+{
+    [xml]$xml = Get-Content $xmlfilename
+    $StringWriter = New-Object System.IO.StringWriter
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
+    if ($indent)
+    {
+        $xmlWriter.Formatting = "Indented"
+        $xmlWriter.Indentation = $indentCount
+    }
+    else
+    {
+        $xmlWriter.Formatting = "None"
+    }
+
+    $xml.WriteContentTo($XmlWriter)
+    $XmlWriter.Flush()
+    $StringWriter.Flush()
+    Write-Output $StringWriter.ToString()
+}
+function prettyXml ([string]$filename, $indentCount = 4)
+{
+    $absFilename = resolve-path $filename
+    $xmlstr = Format-XML $absFilename $true $indentCount
+    $xmlstr | out-file -encoding UTF8 -force $absFilename
+}
+
+function compressXml ([string]$filename)
+{
+    $absFilename = resolve-path $filename
+    $xmlstr = Format-XML $absFilename $false
+    $xmlstr | out-file -encoding UTF8 -force $absFilename
+}
+
+function squeezeXml ([string]$filename) {compressXml $filename }
+function shrinkXml  ([string]$filename) {compressXml $filename }
 function get-ipv4InetAddress()
 {
     return (Test-Connection -ComputerName $env:computername -count 1).IPV4Address.ipaddressTOstring
+}
+
+function dateToLong([string]$yyyymmdd)
+{
+    <#
+    .DESCRIPTION
+    Convert date of form, YYYYMMDD to long number
+
+    .OUTPUTS
+    [long]
+    #>
+
+    "new Date(`"${yyyymmdd}`").getTime()" | node -p
+}
+function longToDate([long] $long)
+{
+    "new Date(${long})" | node -p
+}
+function dateToTime([string] $yyyymmdd)
+{
+    <#
+    .DESCRIPTION
+    Convert date of form, YYYYMMDD to long number
+
+    .OUTPUTS
+    [long]
+    #>
+
+    dateToLong $yyyymmdd
+}
+function timeToDate($long)
+{
+    longToDate $long
+}
+function myip()
+{
+    return (get-ipv4InetAddress)
 }
 
 function hex2Dec( $hexValue )
@@ -65,15 +135,15 @@ function strToHex( [string]$str )
 
 function bytesToStr( $bytes )
 {
-   $str = ''
-   foreach ($byte in $bytes)
-   {
+    $str = ''
+    foreach ($byte in $bytes)
+    {
         $hex = convertToBase $byte, 16
-        [char][int] $c =  $hex
+        [char][int] $c = $hex
         write-host $c
-   }
+    }
 
-   return $str
+    return $str
 }
 function convert2bin ( $numValue )
 {
@@ -112,7 +182,7 @@ function fixPhoneNumber( [string]$phoneNumber )
 
 function validateFirstname( [string]$name )
 {
-    if (-not ($name  -cmatch $FirstnameRegEx ) )
+    if (-not ($name -cmatch $FirstnameRegEx ) )
     {
         throw "Invalid Firstname: $name"
     }
@@ -120,7 +190,7 @@ function validateFirstname( [string]$name )
 
 function validateLastname( [string]$name )
 {
-    if (-not ($name  -cmatch $LastnameRegEx ) )
+    if (-not ($name -cmatch $LastnameRegEx ) )
     {
         throw "Invalid Lastname: $name"
     }
