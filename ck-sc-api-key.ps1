b638a81ad4297ffd4a7340d6bb1490dbca0d6e8b
Pete Jansz <peterjansz@gmail.com>
2019-04-05 09:10:39 -0700

    Supports username, password to get oauthToken

--- ck-sc-api-key.ps1
+++ ck-sc-api-key.ps1
@@ -2,44 +2,58 @@
 # Author: Pete Jansz, IGT 2018
 param
 (
-    [string]    $proto,
-    [string]    $hostname,
-    [string]    $apipath = "second-chance/site", #"players/self/attributes", #
-    [string]    $keyFile,
-    [switch]    $help,
-    [switch]    $h
+    [string] $proto,
+    [string] $hostname,
+    [string] $h,
+    [string] $password = "Password1",
+    [string] $p = "Password1",
+    [string] $apipath = "second-chance/site",
+    [string] $keyFile,
+    [switch] $help,
+    [string] $username,
+    [string] $u
 )
 
 $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 Set-PSDebug -Off #-Trace 2
-$whereWasI = $pwd
 
 trap [Exception]
 {
-    [Console]::Error.WriteLine($_.Exception);
-    Set-Location $whereWasI
+    [Console]::Error.WriteLine($_.Exception)
 }
 
 $ScriptName = $MyInvocation.MyCommand.Name
 function showHelp()
 {
     Write-Host "Check, validate, test ESA api-keys"
-    Write-Host "USAGE: $ScriptName [option] -proto <http|https> -hostname <hostname> -keyfile <xml or text file>"
-    Write-Host "  [option]"
+    Write-Host "USAGE: $ScriptName [options] <args>"
+    Write-Host "  <args>"
+    Write-Host "      -proto <http|https>"
+    Write-Host "      -h[ostname] <host>"
+    Write-Host "      -u[sername] <username>"
+    Write-Host "      -p[assword] <password default=${password}>"
+    Write-Host "      -keyfile <jboss-env-config.xml or text file>"
+    Write-Host "  [options]"
     Write-Host "      -apipath <path> # Default = ${apipath}"
     exit 1
 }
 
-if ($h -or $help) {showHelp}
-if (-not($proto)) {showHelp}
-if (-not($hostname)) {showHelp}
-if (-not($keyfile)) {showHelp}
-function createHeader($apiKey)
+if ( $help ) { showHelp }
+if (-not($proto)) { showHelp }
+if (-not ($h -or $hostname)) { showHelp }
+if (-not($keyfile)) { showHelp }
+if ($h) { $hostname = $h }
+if ($p) { $password = $p }
+if ($u) { $username = $u }
+
+
+function createHeader($apiKey, $oauthToken)
 {
     $header = @{                                `
             'Content-Type'   = 'application/json'    ; `
             'cache-control'  = 'no-cache'            ; `
+            'authorization'  = ("OAuth " + $oauthToken) ; `
             'x-ex-system-id' = 8             ; `
             'x-channel-id'   = 3              ; `
             'x-site-id'      = 35               ; `
@@ -60,9 +74,15 @@ else
     $keys = Get-Content $keyFile
 }
 
+$port = 80
+if ($proto -match "https")
+{$port = 443}
+
+$oauthToken = pdplayer -h $hostname -port $port -logintoken $username -password $password
+
 foreach ($apiKey in $keys)
 {
     Write-Output $apiKey
-    $header = createHeader $apiKey
+    $header = createHeader $apiKey $oauthToken
     $response = Invoke-WebRequest -uri $uri -Method GET -Headers $header
 }
