ab79474c1974fa2731912ce9c1e22a31fc1e9872
Pete Jansz <peterjansz@gmail.com>
2019-08-08 16:11:08 -0700

    Convert XML functions to support stdin, FileInfo and String filename objects

--- lib-general.ps1
+++ lib-general.ps1
@@ -1,26 +1,65 @@
 <#
     Author: Pete Jansz, IGT, June 2017
 #>
-
-$addressLine = "^[a-zA-Z0-9 /.#-]{1,50}$"
-$addressLineNotRequired = "^[a-zA-Z0-9 /.#-]{0,50}$"
-$EmailRegex = "^[a-zA-Z0-9.!£#$%&'^_`{}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+New-Variable -Name AddressLineRegEx -Option ReadOnly -Value "^[a-zA-Z0-9 /.#-]{1,50}$"
+New-Variable -Name AddressLineNotRequiredRegEx -Option ReadOnly -Value "^[a-zA-Z0-9 /.#-]{0,50}$"
+New-Variable -Name EmailRegex "^[a-zA-Z0-9.!£#$%&'^_`{}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$"
+New-Variable -Name LastnameRegEx -Option ReadOnly -Value "^[A-Za-z\\xE1\\xE9\\xED\\xF3\\xFA\\xC1\\xC9\\xCD\\xD3\\xDA\\xF1\\xD1\\xDC\\xFC ,.'-]{1,30}$"
 $FirstnameRegEx = "^[A-Z ,.'-]{1,30}$"
 $FirstnameRegEx = "^[A-Za-z\\xE1\\xE9\\xED\\xF3\\xFA\\xC1\\xC9\\xCD\\xD3\\xDA\\xF1\\xD1\\xDC\\xFC ,.'-]{1,30}$"
-$LastnameRegEx = "^[A-Za-z\\xE1\\xE9\\xED\\xF3\\xFA\\xC1\\xC9\\xCD\\xD3\\xDA\\xF1\\xD1\\xDC\\xFC ,.'-]{1,30}$"
 
-function rmXmlDeclaration([string]$filename)
+filter my-cat( $file )
 {
-    $absFilename = resolve-path $filename
-    [xml] $xmlDoc = Get-Content ($absFilename)
-    if ($xmlDoc.xml -match 'version=')
+    if ( $file -and ($file.GetType().Name -eq 'String' -or $file.GetType().Name -eq 'FileInfo') )
     {
-        $xmlDoc.RemoveChild($xmlDoc.FirstChild) | out-null
-        $xmlDoc.save($absFilename)
+        $output = Get-Content $file
+    }
+    else # stdin
+    {
+        $tmpFile = New-TemporaryFile
+
+        foreach ($item in $input)
+        {
+            $item | Out-File $tmpFile -Encoding UTF8 -Append
+        }
+
+        $output = Get-Content $tmpFile #| format-json.js
+
+        Remove-Item $tmpFile
+    }
+
+    return $output
+}
+filter lowercase
+{
+    if ($_.GetType().Name -eq "String")
+    {
+        return $_.ToLower()
+    }
+    else
+    {
+        throw "Not a String."
     }
 }
-function Format-XML([string]$xmlfilename, [bool]$indent = $true, [int]$indentCount = 4)
+function Format-XML( [string]$xmlfilename, [bool]$indent = $true, [int]$indentCount = 4 )
 {
+    <#
+    .SYNOPSIS
+    Format XML file
+
+    .DESCRIPTION
+    Format XML file
+
+    .INPUTS
+    [string] xmlfilename
+    [bool] indent
+    [int] indentCount
+
+    .OUTPUTS
+    String of XML content
+
+    #>
+
     [xml]$xml = Get-Content $xmlfilename
     $StringWriter = New-Object System.IO.StringWriter
     $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
@@ -39,22 +78,148 @@ function Format-XML([string]$xmlfilename, [bool]$indent = $true, [int]$indentCou
     $StringWriter.Flush()
     Write-Output $StringWriter.ToString()
 }
-function prettyXml([string]$filename, $indentCount = 4)
+function prettyXml( $file, [bool]$overwrite = $false, [int]$indentCount = 4 )
 {
-    $absFilename = resolve-path $filename
+    <#
+    .SYNOPSIS
+    Format XML as human-readable
+
+    .DESCRIPTION
+    Format XML as human-readable
+
+    .INPUTS
+    stdin or FileInfo or String filename
+    [bool] overwrite file
+    [int] indentCount
+
+    .OUTPUTS
+    String of XML content
+
+    #>
+
+    $tempFile = $null
+
+    if ( $file -and ($file.GetType().Name -eq 'String' -or $file.GetType().Name -eq 'FileInfo') )
+    {
+        $absFilename = (resolve-path $file).Path
+    }
+    else # stdin
+    {
+        $tempFile = New-TemporaryFile
+        $absFilename = (resolve-path $tempFile).Path
+
+        foreach ($item in $input)
+        {
+            $item | Out-File $absFilename -Encoding UTF8 -Append
+        }
+    }
+
     $xmlstr = Format-XML $absFilename $true $indentCount
-    $xmlstr | out-file -encoding UTF8 -force $absFilename
+
+    if ($overwrite)
+    {
+        $xmlstr | out-file -encoding UTF8 -force $absFilename
+    }
+
+    if ($tempFile) { Remove-Item $tempFile }
+
+    return $xmlstr
 }
 
-function compressXml([string]$filename)
+function compressXml( $file, [bool]$overwrite = $false )
 {
-    $absFilename = resolve-path $filename
+    <#
+    .SYNOPSIS
+    Compress XML, remove white-space between elements
+
+    .DESCRIPTION
+    Compress XML, remove white-space between elements
+
+    .INPUTS
+    stdin or FileInfo or String filename
+    [bool] overwrite file
+
+    .OUTPUTS
+    String of XML content
+
+    #>
+
+    $tempFile = $null
+
+    if ( $file -and ($file.GetType().Name -eq 'String' -or $file.GetType().Name -eq 'FileInfo') )
+    {
+        $absFilename = (resolve-path $file).Path
+    }
+    else # stdin
+    {
+        $tempFile = New-TemporaryFile
+        $absFilename = (resolve-path $tempFile).Path
+
+        foreach ($item in $input)
+        {
+            $item | Out-File $absFilename -Encoding UTF8 -Append
+        }
+    }
+
     $xmlstr = Format-XML $absFilename $false
-    $xmlstr | out-file -encoding UTF8 -force $absFilename
+
+    if ($overwrite)
+    {
+        $xmlstr | out-file -encoding UTF8 -force $absFilename
+    }
+
+    if ($tempFile) { Remove-Item $tempFile }
+
+    return $xmlstr
 }
 
-function squeezeXml ([string]$filename) { compressXml $filename }
-function shrinkXml  ([string]$filename) { compressXml $filename }
+function rmXmlDeclaration( $file )
+{
+    <#
+    .SYNOPSIS
+    Remove xml declaration from XML file
+
+    .DESCRIPTION
+    Remove xml declaration from XML file
+
+    .INPUTS
+    stdin or FileInfo or file name
+
+    .OUTPUTS
+    String of XML content
+
+    #>
+
+    $tempFile = $null
+
+    if ( $file -and ($file.GetType().Name -eq 'String' -or $file.GetType().Name -eq 'FileInfo') )
+    {
+        $absFilename = (resolve-path $file).Path
+    }
+    else # stdin
+    {
+        $tempFile = New-TemporaryFile
+        $absFilename = (resolve-path $tempFile).Path
+
+        foreach ($item in $input)
+        {
+            $item | Out-File $absFilename -Encoding UTF8 -Append
+        }
+    }
+
+    [xml] $xmlDoc = Get-Content $absFilename
+    if ($xmlDoc.FirstChild.Value -match 'version=')
+    {
+        $xmlDoc.RemoveChild($xmlDoc.FirstChild) | out-null
+        $xmlDoc.save($absFilename)
+    }
+
+    $xmlStr = Get-Content $absFilename
+    if ($tempFile) { Remove-Item $tempFile }
+
+    return $xmlstr
+}
+
 function get-ipv4InetAddress()
 {
     return (Test-Connection -ComputerName $env:computername -count 1).IPV4Address.ipaddressTOstring
@@ -73,51 +238,117 @@ function dateToLong( [string]$yyyymmdd )
     "new Date(`"${yyyymmdd}`").getTime()" | node -p
 }
 
-<#
-Convert long UnixTimeMilliseconds to JSON passable to .NET DateTime
-Example:
-1565040240000 == 'Monday, August 5, 2019 2:24:00 PM'
-returns '2019-08-05T21:24:00.000Z'
-#>
 function longToJSON( [long] $long )
 {
+    <#
+    .DESCRIPTION
+    Convert long UnixTimeMilliseconds to JSON passable to .NET DateTime
+
+    .INPUTS
+    [long] $long UnixTimeMilliseconds
+
+    .OUTPUTS
+    [string]
+
+    .Example
+    1565040240000 == 'Monday, August 5, 2019 2:24:00 PM'
+    returns '2019-08-05T21:24:00.000Z'
+    #>
+
     "new Date( ${long} ).toJSON()" | node -p
 }
 
-<#
-    Convert long UnixTimeMilliseconds to .NET DateTime
-    1565040240000 == 'Monday, August 5, 2019 2:24:00 PM' == 637006118400000000 ticks
-#>
 function longToDateTime( [long] $long )
 {
+    <#
+    .SYNOPSIS
+    Convert long UnixTimeMilliseconds to .NET DateTime
+
+    .DESCRIPTION
+    Convert long UnixTimeMilliseconds to .NET DateTime
+
+    .INPUTS
+    [long] long UnixTimeMilliseconds
+
+    .OUTPUTS
+    [DateTime]
+
+    .EXAMPLE
+    1565040240000 == 'Monday, August 5, 2019 2:24:00 PM' == 637006118400000000 ticks
+    #>
+
     $jsDate = longToJSON $long
     return [DateTime]::Parse( $jsDate )
 }
 
-<#
-    Convert long UnixTimeMilliseconds to .NET DateTimeOffset
-    1565040240000 == 'Monday, August 5, 2019 2:24:00 PM' == 637006118400000000 ticks
-#>
 function longToDateTimeOffset( [long] $long )
 {
+    <#
+    .SYNOPSIS
+    Convert long UnixTimeMilliseconds to .NET DateTimeOffset
+
+    .DESCRIPTION
+    Convert long UnixTimeMilliseconds to .NET DateTimeOffset
+
+    .INPUTS
+    [long] long UnixTimeMilliseconds
+
+    .OUTPUTS
+    [DateTimeOffset]
+
+    .EXAMPLE
+    1565040240000 == 'Monday, August 5, 2019 2:24:00 PM' == 637006118400000000 ticks
+    #>
+
     [DateTimeOffset]::FromUnixTimeMilliseconds( $long ).ToLocalTime()
 }
 
-<#
-    Convert long UnixTimeMilliseconds to local date-time string, e.g.,
-    1565040240000 == 'Mon Aug 05 2019 14:24:00 GMT-0700 (Pacific Daylight Time)'
-#>
 function longToDate( [long] $long )
 {
+    <#
+    .SYNOPSIS
+    Convert long UnixTimeMilliseconds to local date-time string
+
+    .DESCRIPTION
+    Convert long UnixTimeMilliseconds to local date-time string
+
+    .INPUTS
+    [long] long UnixTimeMilliseconds
+
+    .OUTPUTS
+    [string] localized date-time
+
+    .EXAMPLE
+    1565040240000 == 'Mon Aug 05 2019 14:24:00 GMT-0700 (Pacific Daylight Time)'
+    #>
+
     "new Date(${long}).toString()" | node -p
 }
 
 <#
+    .DESCRIPTION
     Convert long UnixTimeMilliseconds to local date-time string, e.g.,
     1565040240000 == 'Mon Aug 05 2019 14:24:00 GMT-0700 (Pacific Daylight Time)'
 #>
 function timeToDate( $long )
 {
+    <#
+    .SYNOPSIS
+    Convert long UnixTimeMilliseconds to local date-time string
+
+    .DESCRIPTION
+    Convert long UnixTimeMilliseconds to local date-time string
+
+    .INPUTS
+    [long] long UnixTimeMilliseconds
+
+    .OUTPUTS
+    [string] localized date-time
+
+    .EXAMPLE
+    1565040240000 == 'Mon Aug 05 2019 14:24:00 GMT-0700 (Pacific Daylight Time)'
+    #>
+
     longToDate $long
 }
 function dateToTime([string] $yyyymmdd)
@@ -133,12 +364,16 @@ function dateToTime([string] $yyyymmdd)
     dateToLong $yyyymmdd
 }
 
-<#
-    Convert dates of formats ('yyyy-MM-dd hh:mm:ss', 'yyyy-MM-dd:hh:mm:ss')
-    to System.DateTime
-#>
 function convertLog4JDateToDateTime( [String] $logDate )
 {
+    <#
+        .DESCRIPTION
+        Convert dates of formats ('yyyy-MM-dd hh:mm:ss', 'yyyy-MM-dd:hh:mm:ss')
+        to System.DateTime
+
+        .OUTPUTS
+        [DateTime]
+    #>
 
     New-Variable -Name DateRegEx -Option ReadOnly -Value '^20[0-9]{2}-[0-9]{2}-[0-9]{2}'
     New-Variable -Name TimeRegEx -Option ReadOnly -Value '[0-9]{2}:[0-9]{2}:[0-9]{2}'
@@ -181,7 +416,7 @@ function hex2Dec( $hexValue )
     return $decValue
 }
 
-function hex2dec2 ($hexValue )
+function hex2dec2( $hexValue )
 {
     return [Convert]::ToInt64( $hexValue, 16 )
 }
@@ -208,11 +443,12 @@ function bytesToStr( $bytes )
     {
         $hex = convertToBase $byte, 16
         [char][int] $c = $hex
-        write-host $c
+        $str += $c
     }
 
     return $str
 }
+
 function convert2bin ( $numValue )
 {
     return convertToBase $numValue 2
@@ -274,15 +510,14 @@ function validateEmailAddress( [string] $s )
 
 function validateAddress( [string] $s )
 {
-    if (-not  ($s -cmatch $addressLine ) )
+    if (-not  ($s -cmatch $AddressLineRegEx ) )
     {
         throw "Invalid Address: $s"
     }
 }
-
 function validateAddress2( [string] $s )
 {
-    if (-not  ($s -cmatch $addressLineNotRequired ) )
+    if (-not  ($s -cmatch $AddressLineNotRequiredRegEx ) )
     {
         throw "Invalid Address2: $s"
     }
